aa78a8a11009a04810d9e6c02d7dfb4b9da27792
Abrham Tilksew <abrham.tilksew@aastustudent.edu.et>
2025-08-15 09:45:49 +0300

    daily probem challenge

--- B_Karen_and_Coffee.py
+++ B_Karen_and_Coffee.py
@@ -1,4 +1,4 @@
-freq_count = [0]* 200000
+freq_count = [0]* 200005
 r,k,q = map(int, input().split())
 for _ in range(r):
     l,r = map(int, input().split())
@@ -9,22 +9,24 @@ for i in range(1, len(freq_count)):
     freq_count[i] += freq_count[i-1]
 
 # step2: another prefix sum 
-for i in range(len(freq_count)):
+for i in range(len(freq_count)):      
     if freq_count[i] < k:
-          freq_count[i] = 0 
+        freq_count[i] = 0 
     else:
-         freq_count[i] = 1
+        freq_count[i] = 1
 
-# print(freq_count)
-# prefix_sum 
 for i in range(1,len(freq_count)):
-     freq_count[i] += freq_count[i-1]
+    freq_count[i] += freq_count[i-1]
+
+for _ in range(q):
+    l,r  = map(int, input().split())
+    if l == 0:
+        print(freq_count[r])
+    else:
+        print(freq_count[r] - freq_count[l-1])
+     
     
 
-print(freq_count[94])
-print(freq_count[97])
-print(freq_count[96])
-print(freq_count[100])
 
 
     
